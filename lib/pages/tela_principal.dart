1c53e3c144197ca4159b4118c8ce3d555c515e79
MikaCintra <mikaellacintrav@icloud.com>
2025-06-26 19:39:04 -0300

    remove telaprincipal

--- lib/pages/tela_principal.dart
+++ lib/pages/tela_principal.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/material.dart';
-import 'package:pj_mobile/pages/tela_review.dart';
+// import 'package:flutter/material.dart';
+// import 'package:pj_mobile/pages/tela_review.dart';
 
-class TelaPrincipal extends StatefulWidget {
-  const TelaPrincipal({super.key});
+// class TelaPrincipal extends StatefulWidget {
+//   const TelaPrincipal({super.key});
 
-  @override
-  State<StatefulWidget> createState() => _TelaPrincipal();
-}
+//   @override
+//   State<StatefulWidget> createState() => _TelaPrincipal();
+// }
 
-class _TelaPrincipal extends State<TelaPrincipal> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => TelaReview()),
-      );
-    });
-  }
+// class _TelaPrincipal extends State<TelaPrincipal> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (_) => TelaReview()),
+//       );
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
